75372b0ddb2bcaae64215bfbe097a36373e19ef5
eilla1 <git@ella.cx>
2021-08-31 12:08:56 -0700

    Add button to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import styles from "../styles/Home.module.scss";
-
+import Link from "next/link";
 export default function Home() {
   return (
     <div className={styles.container}>
       <p>Hours Takehome project</p>
+      <Link href="/start" passHref>
+        <button>Start a session</button>
+      </Link>
     </div>
   );
 }
